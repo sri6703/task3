b2918d7cfe28f43676496c437b48fdcbf7b2d173
sri6703 <cb.en.u4cse20362@cb.students.amrita.edu>
2024-01-14 04:57:38 +0530

    dd

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const port = 3000 ;
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
-
+ 
 mongoose.connect("mongodb://127.0.0.1:27017/kaiburr")
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:',err));
